609ad198f8fe5289826f0bec3dfcdb2c36ebd3d9
parik-shit <parikshitnasa@yahoo.in>
2024-08-05 19:55:00 +0530

    added gifs

--- src/components/SocialMediaButtons.js
+++ src/components/SocialMediaButtons.js
@@ -20,49 +20,77 @@ const SocialMediaButtons = ({ handleTilt }) => {
 
   return (
     <div className="flex flex-col items-center mt-8 relative overflow-y-auto">
+      {/* GIF Containers */}
+      <div className="gif-container left-gif">
+        <iframe src="https://giphy.com/embed/j0MxtY0mrRTV0VqKPn" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+      </div>
+      <div className="gif-container right-gif">
+        <iframe src="https://giphy.com/embed/j0MxtY0mrRTV0VqKPn" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+      </div>
+
       <button onClick={() => { handleShowContent(); handleAudioPlay(); }} className="bg-black text-white font-bold py-4 px-8 rounded mb-4">
-        Tickle My Balls
-      </button>
+    Feed $bella  
+    </button>
       {showContent && (
         <>
+<div className="flex flex-col bg-gray-300 rounded-3xl items-center w-max p-3 mb-4">
+  {/* Moonshot Button */}
+  <div className="w-full px-10 mb-2">
+    <button className="moonshot-button bg-black hover:bg-red-600 text-white font-bold rounded-xl py-2 px-4 w-full">
+      Moonshot
+    </button>
+  </div>
+
+  {/* Buttons in a Row */}
+  <div className="button-row flex flex-row">
+    <a href="https://t.me/TADUMPORTAL" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+        Telegram
+      </button>
+    </a>
+    <a href="https://twitter.com/tudumfornutz" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+        Contract Address
+      </button>
+    </a>
+    <a href="https://t.me/TADUMPORTAL" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+        Twitter
+      </button>
+    </a>
+  </div>
+</div>
+
           <div className="flex-container">
-            <p className="custom-text flex-item bg-orange-100 p-3 rounded-full">
+            <p className="custom-text flex-item bg-orange-100 p-4 rounded-full">
               $bella wants you to follow her on telegram and twitter and if you like invest on her cuteness on the contact address
             </p>
           </div>
-          <div className="flex-container rounded-full bg-gray-300 p-3">
-            <a href="https://t.me/TADUMPORTAL" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-              <button className="bg-black hover:bg-red-600 text-white font-bold  rounded-full py-2 px-4 rounded ">Telegram</button>
-            </a>
-            <a href="https://twitter.com/tudumfornutz" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-              <button className="bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded rounded-full ">Twitter</button>
-            </a>
-            <a href="https://t.me/TADUMPORTAL" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-              <button className="bg-black hover:bg-red-600 text-white font-bold py-2 px-4 rounded  rounded-full ">Contract Address</button>
-            </a>
-          </div>
+          
           <div className="flex-container">
-            <p className="custom-text flex-item  bg-orange-100 p-3 rounded-full">
+            <p className="custom-text flex-item bg-orange-100 p-3 rounded-full">
               $bella
             </p>
             <img src="/bella.png" alt="Ball Image" className="half-size flex-item" />
           </div>
+
           <div className="flex-container">
             <img src="/Bella_wifhat.png" alt="Second Image" className="half-size flex-item" />
-            <p className="custom-text flex-item  bg-orange-100 p-3 rounded-full">
+            <p className="custom-text flex-item bg-orange-100 p-3 rounded-full">
               $bellawifhat
             </p>
           </div>
+
           <div className="flex-container">
-            <p className="custom-text flex-item  bg-orange-100 p-3 rounded-full">
-              $bella wants to meet new holders slurps
+            <p className="custom-text flex-item bg-orange-100 p-3 rounded-full">
+              $bella want to meet new holders slurps
             </p>
             <img src="/Bella_slurping.png" alt="First Image" className="half-size flex-item" />
           </div>
         </>
       )}
       {audioPlaying && (
-        <audio src="/audio.mp3" autoPlay onEnded={handleAudioPause} />
+        <audio src="/audio_bark.mp3" autoPlay onEnded={handleAudioPause} />
       )}
     </div>
   );
