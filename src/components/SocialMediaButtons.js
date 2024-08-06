6342924473f637c350abc8a8452e9a929d6ccd03
Vatsal Chaturvedi <vatsalchaturvedi278@gmail.com>
2024-08-06 17:02:18 +0000

    button changed

--- src/components/SocialMediaButtons.js
+++ src/components/SocialMediaButtons.js
@@ -5,6 +5,8 @@ const SocialMediaButtons = ({ handleTilt }) => {
   const [showContent, setShowContent] = useState(false);
   const [audioPlaying, setAudioPlaying] = useState(false);
 
+  const walletAddress = 'aybntGU52SLmgaSbrg4v1a6FiyMKWEEECgXXKndA4Pr';
+
   const handleShowContent = () => {
     setShowContent(true);
     handleTilt();
@@ -18,6 +20,16 @@ const SocialMediaButtons = ({ handleTilt }) => {
     setAudioPlaying(false);
   };
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(walletAddress)
+      .then(() => {
+        alert('Wallet address copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Could not copy text: ', err);
+      });
+  };
+
   return (
     <div className="flex flex-col items-center mt-8 relative overflow-y-auto">
       {/* GIF Containers */}
@@ -29,40 +41,37 @@ const SocialMediaButtons = ({ handleTilt }) => {
       </div>
 
       <button onClick={() => { handleShowContent(); handleAudioPlay(); }} className="bg-black text-white font-bold py-4 px-8 rounded mb-4">
-    Feed $bella  
-    </button>
+        Feed $bella
+      </button>
       {showContent && (
         <>
-<div className="flex flex-col bg-gray-300 rounded-3xl items-center w-max p-3 mb-4">
-  {/* Moonshot Button */}
-  <div className="w-full px-10 mb-2">
-    <a href="" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-      <button className="moonshot-button bg-black hover:bg-red-600 text-white font-bold rounded-xl py-2 px-4 w-full">
-        Moonshot
-      </button>
-    </a>
-    
-  </div>
+          <div className="flex flex-col bg-gray-300 rounded-3xl items-center w-max p-3 mb-4">
+            {/* Moonshot Button */}
+            <div className="w-full px-10 mb-2">
+              <a href="https://dexscreener.com/solana/aybntgu52slmgasbrg4v1a6fiymkweeecgxxknda4pr" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+                <button className="moonshot-button bg-black hover:bg-red-600 text-white font-bold rounded-xl py-2 px-4 w-full">
+                  Moonshot
+                </button>
+              </a>
+            </div>
 
-  {/* Buttons in a Row */}
-  <div className="button-row flex flex-row">
-    <a href="https://t.me/bellaonsolanaa" target="_blank" rel="noopener noreferrer" एlassName="mx-2 flex-item">
-      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
-        Telegram
-      </button>
-    </a>
-    <a href="" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
-        Contract Address
-      </button>
-    </a>
-    <a href="https://twitter.com/tudumfornutz" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
-      <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
-        Twitter
-      </button>
-    </a>
-  </div>
-</div>
+            {/* Buttons in a Row */}
+            <div className="button-row flex flex-row">
+              <a href="https://t.me/bellaonsolanaa" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+                <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+                  Telegram
+                </button>
+              </a>
+              <button onClick={copyToClipboard} className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+                $bella's DNA
+              </button>
+              <a href="https://twitter.com/tudumfornutz" target="_blank" rel="noopener noreferrer" className="mx-2 flex-item">
+                <button className="bg-black hover:bg-red-600 text-white font-bold rounded-full py-2 px-4">
+                  Twitter
+                </button>
+              </a>
+            </div>
+          </div>
 
           <div className="flex-container">
             <p className="custom-text flex-item bg-orange-100 p-4 rounded-full">
@@ -100,4 +109,3 @@ const SocialMediaButtons = ({ handleTilt }) => {
 };
 
 export default SocialMediaButtons;
-
